466d6375f188e5f4b235e4da172356cb1a883ac3
Toufupi <byte2016@outlook.com>
2021-02-25 21:03:50 +0800

    add wifi connect

--- 2.SourceCode/DigitalClock/DigitalClock.ino
+++ 2.SourceCode/DigitalClock/DigitalClock.ino
@@ -1,16 +1,17 @@
 #include <NTPClient.h>
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
+#include <wifi_link_tool.h>
+
+#define FS_CONFIG
 
 #define DataPin D6
 #define ClockPin D0
 #define Latch D5
 #define OE D7
 
-const char *ssid     = "GOCLOUD";
-const char *password = "1234567890";
 const char *NtpServer = "ntp.aliyun.com";
-int TimeZone = 8;
+int TimeZone = 8;  //时区设置
 int RefreshRate = 10; //in minutes
 
 WiFiUDP ntpUDP;
@@ -22,28 +23,32 @@ int minute1 = 0;
 int minute2 = 0;
 
 byte num[10]={119,17,107,91,29,94,126,19,127,95}; //数字0到9
-int brightness = 800; //亮度值范围为0-1023，1023最暗，0最亮
+int brightness = 900; //亮度值范围为0-1023，1023最暗，0最亮
 byte dot = 1;
-long times = 0;
-long RefreshTime = 0;
-byte starts[7]= {2,1,16,64,32,4,8};
+long times = 0;   //用于显示刷新
+long RefreshTime = 0;  //用于数值刷新
+byte starts[7]= {2,1,16,64,32,4,8}; //LED闪烁一周
+
+void gettool();
+void loadingAnimation();
 
 void setup() {
   Serial.begin(115200);
-  WiFi.begin(ssid, password);
-  while ( WiFi.status() != WL_CONNECTED ) {
-    delay ( 500 );
-    Serial.print ( "." );
-  }
+  rstb = 0;
+  stateled=2;
+  Hostname = "ESP";
+  wxscan=true;
+  load();
+  webServer.on("/gettool", gettool);
+  
   timeClient.begin();
-
   pinMode(DataPin,OUTPUT);
   pinMode(ClockPin,OUTPUT);
   pinMode(Latch,OUTPUT);
   pinMode(OE,OUTPUT);
   analogWrite(OE,brightness);
   times = millis();
-  RefreshTime = millis();
+  
   for(int i = 0;i<8;i++){
   digitalWrite(Latch,LOW);
   shiftOut(DataPin,ClockPin,LSBFIRST,starts[i]);
@@ -54,25 +59,19 @@ void setup() {
   delay(400);
   }
   
+  loadingAnimation();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-/*
-  for(int i=0;i<10;i++){
-  digitalWrite(Latch,LOW);
-  shiftOut(DataPin,ClockPin,LSBFIRST,num[i]);
-  digitalWrite(Latch,HIGH);
-  delay(1000);
-  }
-*/
-  timeClient.update();
+  pant();
   
-  if(RefreshTime - millis()>=30000){
+  if(millis() - RefreshTime>=1000*10 || RefreshTime ==0){
+    timeClient.update();
     hour1 = timeClient.getHours()/10;
     hour2 = timeClient.getHours()%10;
     minute1 = timeClient.getMinutes()/10;
     minute2 = timeClient.getMinutes()%10;
+    RefreshTime = millis();
   }
   
   if(millis()-times >= 1000){
@@ -86,4 +85,60 @@ void loop() {
     digitalWrite(Latch,HIGH);
     //时间每秒刷新一次。
   }
+}
+
+
+
+void gettool() {
+  if(webServer.arg("gettool") == "call") {
+    //当有请求gettool参数是call激活请求返回ojbk
+    Serial.println("get");
+    //串口输出
+    webServer.send(200, "text/plain", "ojbk");
+  }
+}
+
+
+void loadingOne(){
+  digitalWrite(Latch,LOW);
+    shiftOut(DataPin,ClockPin,LSBFIRST,0);
+    shiftOut(DataPin,ClockPin,LSBFIRST,8);
+    shiftOut(DataPin,ClockPin,LSBFIRST,0);
+    shiftOut(DataPin,ClockPin,LSBFIRST,0);
+  digitalWrite(Latch,HIGH);
+}
+void loadingTwo(){
+  digitalWrite(Latch,LOW);
+    shiftOut(DataPin,ClockPin,LSBFIRST,0);
+    shiftOut(DataPin,ClockPin,LSBFIRST,128);
+    shiftOut(DataPin,ClockPin,LSBFIRST,0);
+    shiftOut(DataPin,ClockPin,LSBFIRST,0);
+  digitalWrite(Latch,HIGH);
+}
+void loadingThree(){
+  digitalWrite(Latch,LOW);
+    shiftOut(DataPin,ClockPin,LSBFIRST,0);
+    shiftOut(DataPin,ClockPin,LSBFIRST,0);
+    shiftOut(DataPin,ClockPin,LSBFIRST,8);
+    shiftOut(DataPin,ClockPin,LSBFIRST,0);
+  digitalWrite(Latch,HIGH);
+}
+void loadingFour(){
+  digitalWrite(Latch,LOW);
+    shiftOut(DataPin,ClockPin,LSBFIRST,0);
+    shiftOut(DataPin,ClockPin,LSBFIRST,0);
+    shiftOut(DataPin,ClockPin,LSBFIRST,128);
+    shiftOut(DataPin,ClockPin,LSBFIRST,0);
+  digitalWrite(Latch,HIGH);
+}
+
+void loadingAnimation(){
+  loadingOne();
+  delay(1000);
+  loadingTwo();
+  delay(1000);
+  loadingThree();
+  delay(1000);
+  loadingFour();
+  delay(1000);
 }
--- 2.SourceCode/SourceCode.md
+++ 2.SourceCode/SourceCode.md
@@ -1,6 +1,9 @@
-本项目使用Arduino IDE编写 \n
+本项目使用Arduino IDE编写
+
 使用三个三方库
+
 需要安装ESP库支持
-<h1>使用到的第三方库\n
-    Wifi_link_tool 用于实现wifi配网
-  
+
+### 使用到的第三方库：
+
+wifi_link_tool 用于wifi配网，进行了小幅度修改
